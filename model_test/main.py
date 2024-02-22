c5c4c1243bb6eedd5b7d825ee8b1fc821d25f969
Robert Thompson <rbt61690@gmail.com>
2024-02-22 15:15:15 -0800

    making the model work

--- model_test/main.py
+++ model_test/main.py
@@ -11,14 +11,18 @@ model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-small")
 
 # input_text = "generate: Write a poem about Ireland"
 # input_text = "question: what is 1+1"
-# input_text = "summarize: This is a long article about the history of Rome. Rome was founded in 753 BC by Romulus..."
+# input_text = "summarize: This is a long article about the history of Rome. Rome was founded in 753 BC by Romulus and then Ceasar died and they had Aqueducts and they crossed Rubicon"
 input_text = "translate this to French: This is not a pipe"
 input_ids = tokenizer.encode(input_text, return_tensors="pt")
 
 # Generate the output
-output = model.generate(input_ids, max_length=150, num_return_sequences=1)
+output_1 = model.generate(input_ids, max_length=150, num_return_sequences=1)
+output_2 = model.generate(input_ids, max_length=150, num_return_sequences=1)
+o = .6*output_2 + .4*output_1
+# print('RAW OUTPUT 1 --- ', output_1*6)
+print('RAW OUTPUT 2 --- ', o)
 
 # Decode the generated text
-decoded_output = tokenizer.decode(output[0], skip_special_tokens=True)
-print('INPUT --- ', input_text)
+decoded_output = tokenizer.decode(o[0], skip_special_tokens=True)
+# print('INPUT --- ', input_text)
 print('OUTPUT --- ', decoded_output)
