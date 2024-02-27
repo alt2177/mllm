b1d563e5493c5ffab0be057f31bbe8a53b1a32df
Robert Thompson <rbt61690@gmail.com>
2024-02-27 13:50:44 -0800

    larger model

--- model_test/main.py
+++ model_test/main.py
@@ -1,28 +1,25 @@
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 # Load the tokenizer
-tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-small")
+tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-xl")
 
 # Load the model
-model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-small")
+model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-xl")
 
 # def ask(tasktext):
 
 
 # input_text = "generate: Write a poem about Ireland"
-# input_text = "question: what is 1+1"
+input_text = "question: what is 1+1"
 # input_text = "summarize: This is a long article about the history of Rome. Rome was founded in 753 BC by Romulus and then Ceasar died and they had Aqueducts and they crossed Rubicon"
-input_text = "translate this to French: This is not a pipe"
+# input_text = "translate this to French: I want to learn english. What is the best way to do that?"
 input_ids = tokenizer.encode(input_text, return_tensors="pt")
 
 # Generate the output
-output_1 = model.generate(input_ids, max_length=150, num_return_sequences=1)
-output_2 = model.generate(input_ids, max_length=150, num_return_sequences=1)
-o = .6*output_2 + .4*output_1
-# print('RAW OUTPUT 1 --- ', output_1*6)
-print('RAW OUTPUT 2 --- ', o)
+output = model.generate(input_ids, max_length=150, num_return_sequences=1)
+print('RAW OUTPUT --- ', output)
 
 # Decode the generated text
-decoded_output = tokenizer.decode(o[0], skip_special_tokens=True)
-# print('INPUT --- ', input_text)
+decoded_output = tokenizer.decode(output[0], skip_special_tokens=True)
+print('INPUT --- ', input_text)
 print('OUTPUT --- ', decoded_output)
