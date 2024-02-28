98e6ae20270b8537309ba35d3baa4587a3ef910c
Austin Tao <60298040+alt2177@users.noreply.github.com>
2024-02-27 16:02:35 -0800

    created stuff

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+
+import pyxet         # make xet:// protocol available   
+import pandas as pd 
+import polars as pl  # faster alternative to pandas
+import numpy as np
+import pyarrow
+import matplotlib.pyplot as plt
+import smd
+
 def main():
+    fs = pyxet.XetFS()
     print("hello sheep")
 
 
