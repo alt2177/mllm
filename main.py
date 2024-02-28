2503f0e0ab39a8d3c56212484f6bdca67b9546b0
Austin Tao <60298040+alt2177@users.noreply.github.com>
2024-02-27 16:31:24 -0800

    got imports to work

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ import polars as pl  # faster alternative to pandas
 import numpy as np
 import pyarrow
 import matplotlib.pyplot as plt
-import smd
+from smd.data.load_drug_data import load_drug_data
 
 def main():
-    fs = pyxet.XetFS()
-    print("hello sheep")
-
+    df_training, df_testing = load_drug_data()
+    print(df_training.head())
 
 
 if __name__ == "__main__":
