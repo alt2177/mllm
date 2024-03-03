ca457d8cb9c5fc88a3bf9bd6b070dccd29f25e16
Austin Tao <60298040+alt2177@users.noreply.github.com>
2024-03-03 13:25:40 -0800

    updating notebooks for presentation figures

--- mllm/__init__.py
+++ mllm/__init__.py
@@ -1,6 +1,10 @@
 from mllm.data import load_drug_data, clean_data 
 <<<<<<< HEAD
+<<<<<<< HEAD
 from mllm.core.mergekit import MLLM
 =======
 from mllm.core import MLLM
 >>>>>>> 4565eef23 (began trying to use mergekit in our data pipeline)
+=======
+from mllm.core.mergekit import MLLM
+>>>>>>> a63225b39 (updating notebooks for presentation figures)
--- mllm/core/__init__.py
+++ mllm/core/__init__.py
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 from mllm.core import mergekit 
 =======
 from mllm.core import MLLM
 >>>>>>> 4565eef23 (began trying to use mergekit in our data pipeline)
+=======
+from mllm.core import mergekit 
+>>>>>>> a63225b39 (updating notebooks for presentation figures)
