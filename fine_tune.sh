a111345b9c4ac99a0bb23a91f8b732033d737625
alt2177 <austinhe1@gmail.com>
2024-03-03 18:15:37 -0800

    debugging fine_tune.sh

--- fine_tune.sh
+++ fine_tune.sh
@@ -7,6 +7,7 @@
 #SBATCH --nodelist=saruman
 #SBATCH --gres=gpu:1                   # Request one GPU
 <<<<<<< HEAD
+<<<<<<< HEAD
 #SBATCH --output=output.log     # Standard output and error log
 <<<<<<< HEAD
 #SBATCH --mail-type=BEGIN,END,FAIL           # Send email on job END and FAIL
@@ -19,13 +20,22 @@
 #SBATCH --mail-type=BEGIN, END, FAIL           # Send email on job END and FAIL
 #SBATCH --mail-user= austin.tao@berkeley.edu, phudish_p@berkeley.edu, sean_mcavoy@berkeley.edu, robert_thompson@berkeley.edu 
 >>>>>>> 82f519b2f (created sbatch file)
+<<<<<<< HEAD
 >>>>>>> 7888da94f (created sbatch file)
+=======
+=======
+#SBATCH --output=output.log     # Standard output and error log
+#SBATCH --mail-type=END,FAIL           # Send email on job END and FAIL
+#SBATCH --mail-user=austin.tao@berkeley.edu,phudish_p@berkeley.edu,sean_mcavoy@berkeley.edu,robert_thompson@berkeley.edu 
+>>>>>>> 9e63d322e (debugging fine_tune.sh)
+>>>>>>> cbe69ee22 (debugging fine_tune.sh)
 
 # Load any modules and set up your environment
 module load python/3.8
 module load cuda/10.1                 # Load CUDA module, if required
 
 # run script via Makefile
+<<<<<<< HEAD
 <<<<<<< HEAD
 pip uninstall transformers
 pip install transformers==4.28.0
@@ -47,4 +57,19 @@ python main.py
 =======
 make run
 >>>>>>> 82f519b2f (created sbatch file)
+<<<<<<< HEAD
 >>>>>>> 7888da94f (created sbatch file)
+=======
+=======
+pip uninstall transformers
+pip install transformers==4.28.0
+pip install torch 
+pip install datasets
+pip install evaluate
+pip install numpy
+pip install huggingface
+echo $(pwd)
+python3 ./main.py
+#make run
+>>>>>>> 9e63d322e (debugging fine_tune.sh)
+>>>>>>> cbe69ee22 (debugging fine_tune.sh)
