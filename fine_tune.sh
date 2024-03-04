bdff1bf96333e8b08a080cf0eb7d81a7fb95cb71
Robert Thompson <robert_thompson@scf.berkeley.edu>
2024-03-03 23:17:11 -0800

    starting

--- fine_tune.sh
+++ fine_tune.sh
@@ -6,28 +6,18 @@
 #SBATCH --partition=jsteinhardt        # Specify the partition to run on
 #SBATCH --nodelist=saruman
 #SBATCH --gres=gpu:1                   # Request one GPU
-<<<<<<< HEAD
-<<<<<<< HEAD
 #SBATCH --output=output.log     # Standard output and error log
 #SBATCH --mail-type=END,FAIL           # Send email on job END and FAIL
 #SBATCH --mail-user=robert_thompson@berkeley.edu
-=======
-#SBATCH --output=my_gpu_job_%j.log     # Standard output and error log
-#SBATCH --mail-type=BEGIN, END, FAIL           # Send email on job END and FAIL
-#SBATCH --mail-user= austin.tao@berkeley.edu, phudish_p@berkeley.edu, sean_mcavoy@berkeley.edu, robert_thompson@berkeley.edu 
->>>>>>> 82f519b2f (created sbatch file)
-=======
 #SBATCH --output=output.log     # Standard output and error log
 #SBATCH --mail-type=END,FAIL           # Send email on job END and FAIL
 #SBATCH --mail-user=austin.tao@berkeley.edu,phudish_p@berkeley.edu,sean_mcavoy@berkeley.edu,robert_thompson@berkeley.edu 
->>>>>>> 9e63d322e (debugging fine_tune.sh)
 
 # Load any modules and set up your environment
 module load python/3.8
 module load cuda/10.1                 # Load CUDA module, if required
 
 # run script via Makefile
-<<<<<<< HEAD
 <<<<<<< HEAD
 pip uninstall transformers
 pip install transformers==4.28.0
@@ -43,18 +33,3 @@ git config user.name "mllm-dev"
 echo $(pwd)
 #python -m torch.distributed.launch --nproc_per_node=1 main.py
 python main.py
-=======
-make run
->>>>>>> 82f519b2f (created sbatch file)
-=======
-pip uninstall transformers
-pip install transformers==4.28.0
-pip install torch 
-pip install datasets
-pip install evaluate
-pip install numpy
-pip install huggingface
-echo $(pwd)
-python3 ./main.py
-#make run
->>>>>>> 9e63d322e (debugging fine_tune.sh)
