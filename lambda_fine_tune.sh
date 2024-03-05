a30ebd2e0bb9e3a8f1bb7deaa2de8ccc783b93da
mllm-dev <mllm.dev.222@gmail.com>
2024-03-05 14:21:54 -0800

    starting to parallelize with torch

--- lambda_fine_tune.sh
+++ lambda_fine_tune.sh
@@ -9,6 +9,7 @@
 #SBATCH --output=output.log     # Standard output and error log
 #SBATCH --mail-type=END,FAIL           # Send email on job END and FAIL
 #SBATCH --mail-user=robert_thompson@berkeley.edu 
+<<<<<<< HEAD
 
 # run script via Makefile
 #pip uninstall transformers
@@ -22,6 +23,28 @@
 #pip install huggingface_hub
 #git config user.email "mllm.dev.222@gmail.com"
 #git config user.name "mllm-dev"
+=======
+
+# Load any modules and set up your environment
+module load python/3.8
+module load cuda/10.1                 # Load CUDA module, if required
+
+# run script via Makefile
+pip uninstall transformers
+pip install transformers==4.28.0
+pip install torch 
+pip install datasets
+pip install evaluate
+pip install numpy
+pip install huggingface
+pip install regex --upgrade
+pip install huggingface_hub
+git config user.email "mllm.dev.222@gmail.com"
+git config user.name "mllm-dev"
+
+python -m torch.distributed.launch --nproc_per_node=4 main.py
+#python main.py
+>>>>>>> 817d4eeb6 (starting to parallelize with torch)
 
 #python -m torch.distributed.launch --nproc_per_node=4 main.py
 python main.py
