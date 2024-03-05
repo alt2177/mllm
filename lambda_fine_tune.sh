5a60aaa019726407a3850f92e95871d7e336c805
rbtprograms <rbt61690@gmail.com>
2024-03-05 13:39:17 -0800

    lambda fine tune

--- lambda_fine_tune.sh
+++ lambda_fine_tune.sh
@@ -2,13 +2,11 @@
 
 ## Resource Requests
 #SBATCH --job-name=fine_tuning # Job name
-#SBATCH --nodes=1                      # Use one node
-#SBATCH --partition=jsteinhardt        # Specify the partition to run on
-#SBATCH --nodelist=saruman
+#SBATCH --nodes=1# Use one node
+#SBATCH --account=robert
+#SBATCH --partition=lambda        # Specify the partition to run on
 #SBATCH --gres=gpu:1                   # Request one GPU
 #SBATCH --output=output.log     # Standard output and error log
-#SBATCH --mail-type=END,FAIL           # Send email on job END and FAIL
-#SBATCH --mail-user=robert_thompson@berkeley.edu
 
 # Load any modules and set up your environment
 module load python/3.8
@@ -17,7 +15,7 @@ module load cuda/10.1                 # Load CUDA module, if required
 # run script via Makefile
 pip uninstall transformers
 pip install transformers==4.28.0
-pip install torch
+pip install torch 
 pip install datasets
 pip install evaluate
 pip install numpy
@@ -29,3 +27,4 @@ git config user.name "mllm-dev"
 echo $(pwd)
 #python -m torch.distributed.launch --nproc_per_node=1 main.py
 python main.py
+
