14f5756605612c8a965f1d284706d54cbb98bcca
mllm-dev <mllm.dev.222@gmail.com>
2024-03-06 15:08:05 -0800

    added_yelp_probability

--- merge_output/yelp_dataset/yelp_fine_tune_1.sh
+++ merge_output/yelp_dataset/yelp_fine_tune_1.sh
@@ -7,7 +7,7 @@
 #SBATCH --partition=lambda        # Specify the partition to run on
 #SBATCH --gres=gpu:6                   # Request one GPU
 #SBATCH --output=output_1.log     # Standard output and error log
-#SBATCH --mail-type=START,FAIL,END           # Send email on job END and FAIL
+#SBATCH --mail-type=BEGIN,FAIL,END           # Send email on job END and FAIL
 #SBATCH --mail-user=phudish_p@berkeley.edu 
 
 # run script via Makefile
