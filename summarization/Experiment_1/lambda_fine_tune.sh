e60582ff4d946f2fa3296747ffd1cc6b6d6c4304
PhudishTam <phudishp@gmail.com>
2024-03-05 16:43:47 -0800

    added_summarization

--- summarization/Experiment_1/lambda_fine_tune.sh
+++ summarization/Experiment_1/lambda_fine_tune.sh
@@ -9,19 +9,19 @@
 #SBATCH --output=output.log     # Standard output and error log
 
 # Load any modules and set up your environment
-module load python/3.8
-module load cuda/10.1                 # Load CUDA module, if required
+#module load python/3.8
+#module load cuda/10.1                 # Load CUDA module, if required
 
 # run script via Makefile
-pip uninstall transformers
-pip install transformers==4.28.0
-pip install torch 
-pip install datasets
-pip install evaluate
-pip install numpy
-pip install huggingface
-pip install regex --upgrade
-pip install huggingface_hub
+# pip uninstall transformers
+# pip install transformers==4.28.0
+# pip install torch 
+# pip install datasets
+# pip install evaluate
+# pip install numpy
+# pip install huggingface
+# pip install regex --upgrade
+# pip install huggingface_hub
 git config user.email "mllm.dev.222@gmail.com"
 git config user.name "mllm-dev"
 echo $(pwd)
