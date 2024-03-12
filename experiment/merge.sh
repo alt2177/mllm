b469e2c3ffc8ce839cff777d686956ae33b03d43
mllm-dev <mllm.dev.222@gmail.com>
2024-03-12 13:52:03 -0700

    added necessary files

---
+++ experiment/merge.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+## Resource Requests
+#SBATCH --job-name=fine_tuning # Job name
+#SBATCH --nodes=1                      # Use one node
+#SBATCH --partition=jsteinhardt        # Specify the partition to run on
+#SBATCH --nodelist=saruman
+#SBATCH --gres=gpu:1                   # Request one GPU
+#SBATCH --output=output.log     # Standard output and error log
+#SBATCH --mail-type=END,FAIL           # Send email on job END and FAIL
+#SBATCH --mail-user=sean_mcavoy@berkeley.edu
+
+# run script via Makefile
+#pip uninstall transformers
+git clone https://github.com/cg123/mergekit.git
+cd mergekit
+pip install -e .
+cd ..
+python merge_experiment.py
