bbc0205143f1d382599749bc2637c4e7ef9eac67
mllm-dev <mllm.dev.222@gmail.com>
2024-03-24 21:42:36 -0700

    updated

--- summarization/Experiment_1/billsum_test_2.sh
+++ summarization/Experiment_1/billsum_test_2.sh
@@ -6,7 +6,7 @@
 #SBATCH --account=phudish_p
 #SBATCH --partition=lambda        # Specify the partition to run on
 #SBATCH --gres=gpu:8                   # Request one GPU
-#SBATCH --output=output.log     # Standard output and error log
+#SBATCH --output=output_1.log     # Standard output and error log
 #SBATCH --mail-type=FAIL,END           # Send email on job END and FAIL
 #SBATCH --mail-user=phudish_p@berkeley.edu 
 
@@ -24,4 +24,4 @@
 #git config user.name "mllm-dev"
 #pip install accelerate -U
 #python -m torch.distributed.launch --nproc_per_node=6 main.py
-python gpt2.py
+python gpt2_medium.py
