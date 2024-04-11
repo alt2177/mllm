10e571e1505dd39623a577213ded5a02c64b5c90
mllm-dev <mllm.dev.222@gmail.com>
2024-04-11 15:44:36 -0700

    began running on lambda server

---
+++ test_loss_exp.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+## Resource Requests
+#SBATCH --job-name=test_loss_exp# Job name
+#SBATCH --nodes=1# Use one node
+#SBATCH --account=austin_tao
+#SBATCH --partition=lambda        # Specify the partition to run on
+#SBATCH --gres=gpu:1                   # Request one GPU
+#SBATCH --output=output_test_loss_exp.log     # Standard output and error log
+#SBATCH --mail-type=END,FAIL           # Send email on job END and FAIL
+#SBATCH --mail-user=austin.tao@berkeley.edu
+
+# run script via Makefile
+#pip uninstall transformers
+#pip install transformers==4.28.0
+#pip install accelerate -U
+#pip install transformers
+#pip install torch
+#pip install datasets
+#pip install evaluate
+#pip install numpy
+#pip install huggingface
+#pip install regex --upgrade
+#pip install huggingface_hub
+#git config user.email "mllm.dev.222@gmail.com"
+#git config user.name "mllm-dev"
+#git remote set-url origin https://huggingface.co/mllm-dev/yelp_finetuned_6gpu_full
+python3 test_loss_exp.py
