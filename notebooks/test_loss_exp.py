54000488ae56bbc4bd6766f4691ca627dfc2b580
Austin Tao <60298040+alt2177@users.noreply.github.com>
2024-04-13 12:23:10 -0700

    began visualizing loss probs

--- notebooks/test_loss_exp.py
+++ notebooks/test_loss_exp.py
@@ -25,25 +25,53 @@ fs = pyxet.XetFS()
 # |%%--%%| <zaXx0yw20g|1vPiao2xcN>
 
 # get to correct directory
-os.chdir("../test_loss_exp")
+# os.chdir("./test_loss_exp/")
 
 print(os.getcwd())
 
 
 #|%%--%%| <1vPiao2xcN|KCkw4KfUIQ>
 
-df_test = pl.read_csv("test_probabilities.csv")
-df_val = pl.read_csv("validation_probabilities.csv")
+df_test = pl.read_csv("test_loss_exp/test_probabilities.csv")
+df_val = pl.read_csv("test_loss_exp/validation_probabilities.csv")
+
+# create dataframes with only the probabilities
+df_test_probs = df_test.select(pl.exclude("true_label"))
+df_val_probs = df_val.select(pl.exclude("true_label"))
 
 #|%%--%%| <KCkw4KfUIQ|enSpeRK2EP>
 
 df_test.shape
+df_test.head()
 
 #|%%--%%| <enSpeRK2EP|jphG4SNWO1>
 
-df_test.head()
+# get distribution of max values
 
-#|%%--%%| <jphG4SNWO1|SbuiSHsCrv>
+df_test_maxes = df_test_probs.with_columns(max = pl.max_horizontal(df_test_probs.columns))
+df_val_maxes = df_val_probs.with_columns(max = pl.max_horizontal(df_val_probs.columns))
+
+#|%%--%%| <jphG4SNWO1|vvCOfdMBjs>
+
+
+# Plotting the distribution
+plt.figure(figsize = (10, 6), dpi = 100)
+ax = plt.gca()
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+
+plt.hist(df_test_maxes.select("max"), bins = 30, alpha = 0.75, label = "gpt2-ties-merge (test)", color='lightskyblue')
+plt.hist(df_val_maxes.select("max"), bins = 30, alpha = 0.75, label = "gpt2-ties-merge (validation)", color='violet')
+plt.title('Distribution of Maximum Values per Row')
+plt.xlabel('Max Value')
+plt.ylabel('Frequency')
+plt.grid(True)
+plt.legend()
+plt.savefig("notebooks/images/max_distributions.png")
+plt.show()
+
+#|%%--%%| <vvCOfdMBjs|SbuiSHsCrv>
+
 
 
 
